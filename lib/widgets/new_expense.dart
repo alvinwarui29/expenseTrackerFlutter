d016c14ad2efccca58b9834e16d94b2cba471e6e
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-01 00:49:38 +0300

    store user input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,20 +8,31 @@ class NewExpense extends StatefulWidget {
   }
 }
 
+var titleInput = '';
+void _setTitle(String input) {
+  titleInput = input;
+}
+
 class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
-        children: const [
-          TextField(
+        children: [
+          const TextField(
+            onChanged: _setTitle,
             maxLength: 50,
             keyboardType: TextInputType.name,
             decoration: InputDecoration(
               label: Text('Title'),
             ),
           ),
+          ElevatedButton(
+              onPressed: () {
+                print(titleInput);
+              },
+              child: const Text('Save expense'))
         ],
       ),
     );
