b5381a23035025a4a3eaa9fb3494e0cd83ec3e57
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-30 20:20:01 +0300

    add expense class as stateful widget

---
+++ lib/expenses.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(context) {}
+}
