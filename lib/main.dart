be64b312b0419a6b2001f724c94c5269ad17f2a0
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-01 00:15:24 +0300

    remove the debug tag

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:expensetracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: const Expenses(),
     ),
